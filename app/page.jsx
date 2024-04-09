dd6e8178ab22f0aa465e424b7c8c7217118fbdc3
Dan Spelt <62000481+danspelt@users.noreply.github.com>
2024-04-08 20:20:28 -0700

    Update Dropzone styling

--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,7 @@ import Dropzone from '@components/Dropzone';
 const Home = () => {
   return ( 
     <div className='flex justify-center items-center h-screen w-screen bg-sky-400'>
-      <Dropzone className='h-screen w-screen bg-white' />
+      <Dropzone className='h-screen w-screen absolute bg-violet-50 z-10 opacity-30' />
       <View3D />
     </div>
    );
