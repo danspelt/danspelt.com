b3ed71799424221fad457267ea372dd444f6012f
Dan Spelt <62000481+danspelt@users.noreply.github.com>
2024-04-16 16:02:59 -0700

    Remove unused files and dependencies

--- app/redux/Slices/index.js
+++
@@ -1,7 +0,0 @@
-import { AssistantSlice } from "./AssistanSlice";
-import { ThreadSlice } from "./ThreadSlice";
-import { MessageSlice } from "./MessageSlice";
-import {DataSlice} from  "./DataSlice"
-
-
-export { AssistantSlice, ThreadSlice, MessageSlice, DataSlice };
--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    
-}
-
-module.exports = nextConfig
+    reactStrictMode: true,
+    swcMinify: true,
+    webpack(config) {
+      config.experiments = { ...config.experiments, topLevelAwait: true };
+      return config;
+    },
+  };
+  
+  export default nextConfig;
+  
