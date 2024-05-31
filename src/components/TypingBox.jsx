df2be6f3b3876b4aec065b943386e4d309a79916
Dan Spelt <62000481+danspelt@users.noreply.github.com>
2024-05-31 13:55:08 -0700

    chore: Update TypingBox component to display AI Bot's purpose

--- src/components/TypingBox.jsx
+++ src/components/TypingBox.jsx
@@ -14,13 +14,9 @@ export const TypingBox = () => {
     <div className="z-10 max-w-[600px] flex space-y-6 flex-col bg-gradient-to-tr  from-slate-300/30 via-gray-400/30 to-slate-600-400/30 p-4  backdrop-blur-md rounded-xl border-slate-100/30 border">
       <div>
         <h2 className="text-white font-bold text-xl">
-          How to say in Japanese?
+          Ask AI Bot anything about Dan Spelt professional journey
         </h2>
-        <p className="text-white/65">
-          Type a sentence you want to say in Japanese and AI Sensei will
-          translate it for you.
-        </p>
-      </div>
+    </div>
 
       {loading ? (
         <div className="flex justify-center items-center">
